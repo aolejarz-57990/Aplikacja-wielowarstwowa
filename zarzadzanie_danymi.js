01d26887e2d91ba9902269e8330a09281f8b73b0
Aleksandra Olejarz <a.s.olejarz@gmail.com>
2024-04-30 12:37:34 +0200

    Dodano definicje funkcji dostępu do danych

--- zarzadzanie_danymi.js
+++ zarzadzanie_danymi.js
@@ -1,3 +1,52 @@
+// Obsługa kolekcji klientów
+function pobierzKlientow() {
+}
+
+function dodajKlienta(klient) {    
+}
+
+function pobierzKlienta(kleintId) {
+}
+
+function aktualizujKlienta(klient) {
+}
+
+function usunKlienta(klientId) {
+}
+
+// Obsługa kolekcji zabiegów
+function pobierzZabiegi() {
+}
+
+function dodajZabieg(zabieg) {
+}
+
+function pobierzZabieg(zabiegId) {
+}
+
+function aktualizujZabieg(zabieg) {
+}
+
+function usunZabieg(zabiegId) {
+}
+
+// Obsługa zamówień
+
+function pobierzZamowienia() {
+}
+
+function dodajZamowienie(zamowienie) {
+}
+
+function pobierzZamowienie(zamowienieId) {
+}
+
+function aktualizujZamowienie(zamowienie) {
+}
+
+function usunZamowienie(zamowienieId) {
+}
+
 class Klient {
     id;
     Imie = "";
