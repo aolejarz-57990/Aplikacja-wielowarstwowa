4488784bf994ac9994412a8fde6f15fa3a2ec125
Aleksandra Olejarz <a.s.olejarz@gmail.com>
2024-05-29 18:31:15 +0200

    Ununięto testy oparte na Playwright

--- tests/backend/pobierzKlientow.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test("Pobierz klientow", async ({ request, baseURL }) => {
-    const _response = await request.get(`${baseURL}klienci`
-    );
-    expect(_response.ok()).toBeTruthy();
-    expect(_response.status()).toBe(200);
-    console.log(await _response.json());
-  });
