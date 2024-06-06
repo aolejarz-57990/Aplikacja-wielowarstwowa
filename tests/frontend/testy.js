27e851f0f0e6480d1aeb8f5bd0b6388cfcb683a1
Aleksandra Olejarz <a.s.olejarz@gmail.com>
2024-06-06 20:47:51 +0200

    Dodano dwa nowe testy funkcjonalne

--- tests/frontend/testy.js
+++ tests/frontend/testy.js
@@ -18,7 +18,6 @@ async function stronaGlownaTest() {
     }
 
 async function nowyKlientTest() {
-  // Uruchomienie przeglądarki
   let driver = await new Builder().forBrowser("chrome").build();
   try {
     // Przejdź do strony aplikacji
@@ -26,14 +25,65 @@ async function nowyKlientTest() {
     const nowyKlientBtn = await driver.findElement(By.id("btn_dodaj_klienta"));
     await driver.findElement(By.id("btn_klienci")).click();
     await driver.wait(until.elementIsVisible(nowyKlientBtn), 5000);
-    await sleep(2000);
     await driver.findElement(By.id("btn_dodaj_klienta")).click();
     await driver.findElement(By.id("nowy_klient_imie")).sendKeys("Jan");
     await driver.findElement(By.id("nowy_klient_nazwisko")).sendKeys("Kowalski");
     await driver.findElement(By.id("nowy_klient_tel")).sendKeys("123456ABC");
     await driver.findElement(By.id("dodaj_klienta_submit")).click();
     await driver.wait(until.elementIsVisible(nowyKlientBtn), 20000);
-    await sleep(2000);
+    const actualString = await driver.findElement(By.xpath("//*[contains(text(), '123456AB')]")).getText();
+    assert.equal(actualString,"123456AB");
+  } finally {
+    await driver.quit();
+  }
+}
+
+async function dodajIEdytujKlientaTest() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  try {
+    // Przejdź do strony aplikacji
+    await driver.get("https://aolejarz-57990.github.io");
+    // Dodaj klienta
+    const nowyKlientBtn = await driver.findElement(By.id("btn_dodaj_klienta"));
+    await driver.findElement(By.id("btn_klienci")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 5000);
+    await driver.findElement(By.id("btn_dodaj_klienta")).click();
+    await driver.findElement(By.id("nowy_klient_imie")).sendKeys("Jan");
+    await driver.findElement(By.id("nowy_klient_nazwisko")).sendKeys("Nowak");
+    await driver.findElement(By.id("nowy_klient_tel")).sendKeys("111222333");
+    await driver.findElement(By.id("dodaj_klienta_submit")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 20000);
+    await driver.findElement(By.xpath("//*[contains(text(), '111222333')]")).click();
+    await driver.findElement(By.id("szczegoly_nazwisko")).sendKeys("Goszkowski");
+    await driver.findElement(By.id("btn_zapisz_klienci")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 20000);
+    const actualString = await driver.findElement(By.xpath("//*[contains(text(), 'Goszkowski')]")).getText();
+    assert.equal(actualString,"Goszkowski");
+  } finally {
+    await driver.quit();
+  }
+}
+
+async function dodajIUsnunKlientaTest() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  try {
+    // Przejdź do strony aplikacji
+    await driver.get("https://aolejarz-57990.github.io");
+    // Dodaj klienta
+    const nowyKlientBtn = await driver.findElement(By.id("btn_dodaj_klienta"));
+    await driver.findElement(By.id("btn_klienci")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 5000);
+    await driver.findElement(By.id("btn_dodaj_klienta")).click();
+    await driver.findElement(By.id("nowy_klient_imie")).sendKeys("Jan");
+    await driver.findElement(By.id("nowy_klient_nazwisko")).sendKeys("Nowak");
+    await driver.findElement(By.id("nowy_klient_tel")).sendKeys("1122334455X");
+    await driver.findElement(By.id("dodaj_klienta_submit")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 20000);
+    await driver.findElement(By.xpath("//*[contains(text(), '1122334455X')]")).click();
+    await driver.findElement(By.id("btn_usun_klienci")).click();
+    await driver.wait(until.elementIsVisible(nowyKlientBtn), 20000);
+    const actualString = await driver.findElement(By.xpath("//*[contains(text(), '111222333')]")).getText();
+    assert.equal(actualString,"");
   } finally {
     await driver.quit();
   }
@@ -45,3 +95,5 @@ function sleep(ms) {
 
 stronaGlownaTest();
 nowyKlientTest();
+dodajIEdytujKlientaTest();
+dodajIUsnunKlientaTest();
